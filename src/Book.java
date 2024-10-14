259e7be890596dc4cd4c6251034368b71bb76653
VoT <alexxaski@yandex.ru>
2024-10-14 14:41:50 +0300

    hw12go

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+
+    public class Book {
+        private String title;
+        private int yearOfPublication;
+        private Author author;
+
+        public Book(String title, int yearOfPublication, Author author) {
+            this.title = title;
+            this.yearOfPublication = yearOfPublication;
+            this.author = author;
+        }
+
+        public Author getAuthor() {
+            return author;
+        }
+
+        public int getYearOfPublication() {
+            return yearOfPublication;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setYearOfPublication(int yearOfPublication) {
+            this.yearOfPublication = yearOfPublication;
+        }
+    }
